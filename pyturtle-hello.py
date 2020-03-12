0232c17ab2fadd8799e1a5a964fff6ecfb0e9219
Zhang-Jianing <54052427+Zhang-Jianing@users.noreply.github.com>
2020-03-12 08:48:40 +0800

    Add files via upload

---
+++ pyturtle-hello.py
@@ -0,0 +1,13 @@
+import turtle #引入turtle
+turtle.showturtle()
+turtle.write("张家宁")
+turtle.forward(300)
+turtle.color("red")
+turtle.left(90)
+turtle.forward(300)
+turtle.goto(0,50)
+turtle.goto(0,0)
+turtle.penup() #抬笔
+turtle.goto(0,300)
+turtle.pendown()
+turtle.circle(100)#画圆，半径为100
